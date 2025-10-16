e1c3e148820d246ea22918ff534dcc60150b20d1
kcgr0690 <kcgr0690@gmail.com>
2025-10-15 20:17:40 -0400

    Converting sequence to mrna for template & coding strand.

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.getElementById('translateBtn').addEventListener('click', function() {
     console.log('Sequence:', sequence);
     console.log('Type:', type);
 
+    let mrna = '';
+    if (type === 'mrna') {
+        mrna = sequence;
+    } else if (type == 'coding') {
+        mrna = sequence.replace(/T/g, 'U');
+    } else if (type === template) {
+        const reversed = sequence.split('').reverse().join('');
+        const complement = reversed
+        .replace(/A/g, 'tempA')
+        .replace(/T/g, 'A')
+        .replace(/tempA/g, 'T')
+        .replace(/G/g, 'tempG')
+        .replace(/C/g, 'G')
+        .replace(/tempG/g, 'C');
+    }
+
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML = `<p>Sequence entered: <strong>${sequence}</strong> (Type: ${type})</p>`;
 });
