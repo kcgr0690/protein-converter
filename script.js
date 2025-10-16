686f31ff6eaf027f34b371e70d862e1e3c98a520
kcgr0690 <kcgr0690@gmail.com>
2025-10-15 20:27:11 -0400

    Error catching done. Length check done. Starting coding identified, sequence trimmed.

--- script.js
+++ script.js
@@ -12,13 +12,28 @@ document.getElementById('translateBtn').addEventListener('click', function() {
     } else if (type === template) {
         const reversed = sequence.split('').reverse().join('');
         const complement = reversed
-        .replace(/A/g, 'tempA')
-        .replace(/T/g, 'A')
-        .replace(/tempA/g, 'T')
-        .replace(/G/g, 'tempG')
-        .replace(/C/g, 'G')
-        .replace(/tempG/g, 'C');
+            .replace(/A/g, 'tempA')
+            .replace(/T/g, 'A')
+            .replace(/tempA/g, 'T')
+            .replace(/G/g, 'tempG')
+            .replace(/C/g, 'G')
+            .replace(/tempG/g, 'C');
+        mrna = complement.replace(/T/g, 'U');
     }
+
+    if (mrna.length < 3) {
+        outputDiv.innerHTML = '<p>Error: Sequence too short for translation.<\p>';
+        return;
+    }
+
+    const startIndex = mrna.indexOf('AUG');
+    if (startIndex === -1 || startIndex % 3 !== 0) {
+        outputDiv.innerHTML = '<p>Error: No valid "AUG" start codon in reading frame.<\p>';
+        return;
+    }
+
+    const codingMrna = mrna.slice(startIndex);
+    console.log('Coding mRNA from start:', codingMrna);
 
     const outputDiv = document.getElementById('output');
     outputDiv.innerHTML = `<p>Sequence entered: <strong>${sequence}</strong> (Type: ${type})</p>`;
