ab0b804e7e7dc962d6dd0bbd861a645f9a7841fd
kcgr0690 <kcgr0690@gmail.com>
2025-10-15 20:01:15 -0400

    Live server working, console finally working. Was struggling to see the console.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById("translateBtn").addEventListener("click", function() {
-    const sequence = document.getElementById('sequence').ariaValueMax.trim().toUpperCase();
+document.getElementById('translateBtn').addEventListener('click', function() {
+    const sequence = document.getElementById('sequence').value.trim().toUpperCase();
     const type = document.getElementById('type').value;
-    console.log('Sequence: ', sequence);
-    console.log('Type: ', type)
+    console.log('Sequence:', sequence);
+    console.log('Type:', type);
+
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = `<p>Sequence entered: <strong>${sequence}</strong> (Type: ${type})</p>`;
 });
